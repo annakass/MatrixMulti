c81c2edc18f0b7724d7c74aa6a778b2c4eafbbf8
Anna Kassar <you@example.com>
2015-12-27 16:51:07 -0500

    added stuff

---
+++ MatrixMultiP.java
@@ -0,0 +1,41 @@
+public class MatrixMulti {
+	public MatrixMulti (int[][] MatrixA, int[][] MatrixB) {
+		
+	}
+	public static void main (String[] args) {
+		
+	}
+	
+	private class Matrix {
+		private int[][] me; //the matrix
+		private int i; //row number
+		private int j; //column number
+
+	    public Matrix(int[][] matrix) {
+	    	for (int i = 0; matrix[i+1] != null; i++) {
+	    		for (int j = 0; j < matrix.length; j++) {
+	    			
+	    		}
+	    	}
+	    	this.me = matrix;
+	    	this.i = matrix.length;
+	    	
+	    	//new
+	    }
+	  }
+	
+//	private class BoardPlus {
+//	    private Board b;
+//	    private int moves;
+//	    private int manhattan;
+//	    private BoardPlus parent;
+//
+//	    public BoardPlus(Board b, int moves, int manhattan, BoardPlus parent) {
+//	      this.b = b;
+//	      this.moves = moves;
+//	      this.manhattan = manhattan;
+//	      this.parent = parent;
+//	    }
+//	  }
+
+}	
